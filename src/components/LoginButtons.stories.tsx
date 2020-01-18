890f96b9391d6267cd1c6314827ae93a173de5a8
Michael L Perry <michael@qedcode.com>
2020-01-17 21:10:55 -0600

    Define the LoginButtons component.

---
+++ src/components/LoginButtons.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { LoginButtons } from "./LoginButtons";
+
+export default { title: 'Login Buttons' };
+
+export const basic = () => <LoginButtons />
---
+++ src/components/LoginButtons.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const LoginButtons = ({}) => (
+  <div>
+    <p>Log in to comment.</p>
+    <button>Microsoft</button>
+    <button>Google</button>
+    <button>Twitter</button>
+    <button>GitHub</button>
+  </div>
+);
