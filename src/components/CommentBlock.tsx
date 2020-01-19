a2d0d3db382885098978331a20dcba9ce86ed7d2
Michael L Perry <michael@qedcode.com>
2020-01-19 13:31:11 -0600

    Expand and collapse content if it overflows.

--- src/components/CommentBlock.tsx
+++ src/components/CommentBlock.tsx
@@ -8,27 +8,50 @@ export interface CommentBlockProps {
   content: string;
 };
 
-export const CommentBlock = ({ self, content }: CommentBlockProps) => (
-  <div className="jinaga-feedback-comment-block">
-    <div className="jinaga-feedback-comment-block-info">
-      <UserAvatar />
-      <p>{ new Date().toLocaleTimeString() }</p>
-      <p>{ new Date().toLocaleDateString() }</p>
+export const CommentBlock = ({ self, content }: CommentBlockProps) => {
+  const [ expanded, setExpanded ] = React.useState(false);
+  const contentRef = React.useRef<HTMLDivElement>(null);
+  const [ contentHeight, setContentHeight ] = React.useState(0);
+  React.useEffect(() => {
+    setContentHeight(contentRef.current?.scrollHeight ?? 0);
+  }, [contentRef]);
+
+  return (
+    <div className="jinaga-feedback-comment-block">
+      <div className="jinaga-feedback-comment-block-info">
+        <UserAvatar />
+        <p>{ new Date().toLocaleTimeString() }</p>
+        <p>{ new Date().toLocaleDateString() }</p>
+      </div>
+      <div
+        ref={contentRef}
+        className={
+          "jinaga-feedback-comment-block-content" +
+          (expanded
+            ? " jinaga-feedback-comment-block-content-expanded"
+            : " jinaga-feedback-comment-block-content-collapsed")
+        }
+        dangerouslySetInnerHTML={(
+        {
+          __html: content
+        }
+      )}>
+        
+      </div>
+      <div className="jinaga-feedback-comment-block-controls">
+        {
+          self
+            ? <a onClick={action("Edit a comment.")}>Edit</a>
+            : <></>
+        }
+        {
+          contentHeight > 200
+            ? <a onClick={ e => { setExpanded(!expanded); e.preventDefault(); } }>
+                Show { expanded ? "less" : "more" }
+              </a>
+            : <></>
+        }
+      </div>
     </div>
-    <div className="jinaga-feedback-comment-block-content" dangerouslySetInnerHTML={(
-      {
-        __html: content
-      }
-    )}>
-      
-    </div>
-    <div className="jinaga-feedback-comment-block-controls">
-      {
-        self ?
-          <a onClick={action("Edit a comment.")}>Edit</a>
-          : <></>
-      }
-      <a onClick={action("Show more")}>Show more</a>
-    </div>
-  </div>
-)
+  );
+}
